5b47029746f57408712119eb38cc582c76783d7b
Raymond Martinez <rcmartinez1854@gmail.com>
2019-10-08 20:01:31 -0700

    added two new components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import Tooltip from './composition/Tooltip';
 import Messages from './Messages';
 import TheDate from './state/TheDate';
-import Counter from './state/Counter';
+import Counter from './state/Counter.js';
+import HelloWorld from './state-drills/HelloWorld.js'
+import Bomb from './state-drills/Bomb.js'
 
 const firstTooltip = (
   <Tooltip color='hotpink' message='tooltip message'>
@@ -44,7 +46,9 @@ class App extends Component {
         <Messages name="Messages" unread={0}/>
         <Messages name="Notifications" unread={10}/>
         <TheDate />
-        <Counter />
+        <Counter count={123}/>
+        <HelloWorld />
+        <Bomb />
       </div>
     );
   }
