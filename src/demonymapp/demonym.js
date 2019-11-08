13f013b4e5a216fcd021dde35ddd4cfde750985e
Raymond Martinez <rcmartinez1854@gmail.com>
2019-11-07 16:13:23 -0800

    tried new export method

--- src/demonymapp/demonym.js
+++ src/demonymapp/demonym.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './demonym.css';
 
 export default function Demonym(props) {
-    return (
-        <div className="demonym">
-            A {props.name} comes from {props.country}
-        </div>
-    );
+
+  return (
+    <div className="demonym">
+      A {props.name} comes from {props.country}
+    </div>
+  );
 }
